21242d8196afb9661b41811aac5f41b98c114f22
Stepan Nelasov <stepos36@gmail.com>
2021-01-05 23:50:07 -0500

    Added TypeTests

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAnAverageGrade()
         {
             //arrange
             var book = new Book("");
@@ -17,6 +17,7 @@ namespace GradeBook.Tests
             //act
             var result = book.GetStatistics();
 
+            //assert
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High, 1);
             Assert.Equal(77.3, result.Low, 1);
