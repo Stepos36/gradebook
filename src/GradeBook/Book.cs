c80c0961cadf2d515e5e0a1449e435c537f2dd5e
Stepan Nelasov <stepos36@gmail.com>
2021-01-05 22:20:01 -0500

    Module 5: -Added Statistics Class -Refactored Program.cs -Added a Unit test for GetStatistics Method of Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book
+    public class Book
     {
         public Book(string name)
         {
@@ -32,6 +32,23 @@ namespace GradeBook
             Console.WriteLine($"The lowest grade is {lowGrade}");
             Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        public Statistics GetStatistics() 
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            
+            foreach (var grade in grades)
+            {
+                result.Low  = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+            return result;
         }
 
         private List<double> grades;
